9e739d28599566c63eccb94fff094b1d30f460fd
Spike Grobstein <me@spike.cx>
2014-06-11 21:58:27 -0400

    litecoin's directory is 'litecoin''

--- coins/litecoin.rb
+++ coins/litecoin.rb
@@ -6,10 +6,9 @@ module Coffer
       home_page 'https://litecoin.org'
       symbol :LTC
       wallet_executable 'litecoind'
-      directory 'Litecoin'
+      directory 'litecoin'
       config_file 'litecoin.conf'
     end
   end
 end
 
-binding.pry
