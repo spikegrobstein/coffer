cb20a4291b49570cc69e5c25498d541214c0302e
Spike Grobstein <me@spike.cx>
2014-06-11 20:54:53 -0400

    litecoin has a default branch.

--- coins/litecoin.rb
+++ coins/litecoin.rb
@@ -1,6 +1,7 @@
 module Coffer
   module Coin
     class Litecoin < Coffer::Definition
+      git_branch 'master-0.8'
       git_repo 'https://github.com/litecoin-project/litecoin.git'
       home_page 'https://litecoin.org'
       symbol :LTC
